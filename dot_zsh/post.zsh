6fd71e7af1032f48d35ea2f509578b16259149c1
Bin Wang <bin.wang@bhp.com>
2026-05-01 16:06:37 +1000

    kiro cli pre post

---
+++ dot_zsh/post.zsh
@@ -0,0 +1,2 @@
+# Kiro CLI post block. Keep at the bottom of .zsh execution
+[[ -f "${HOME}/.local/share/kiro-cli/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/.local/share/kiro-cli/shell/zshrc.post.zsh"
