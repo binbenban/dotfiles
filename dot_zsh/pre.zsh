cd5f621b91fdb0d9915f726190269b60ca2d293d
binbenban <bin.ben.ban@gmail.com>
2026-05-06 17:29:24 +1000

    Delete dot_zsh/pre.zsh

--- dot_zsh/pre.zsh
+++
@@ -1,2 +0,0 @@
-# Kiro CLI pre block. Keep at the top of .zsh execution
-[[ -f "${HOME}/.local/share/kiro-cli/shell/zshrc.pre.zsh" ]] && builtin source "${HOME}/.local/share/kiro-cli/shell/zshrc.pre.zsh"
