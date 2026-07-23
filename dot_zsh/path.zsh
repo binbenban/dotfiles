1b6ce5cc856e7141e88e85567ec8e4fd30ed77c6
Bin Wang <bin.wang@bhp.com>
2026-07-24 07:51:08 +1000

    path

--- dot_zsh/path.zsh
+++ dot_zsh/path.zsh
@@ -9,6 +9,8 @@ path=(
   $path
 )
 
+[[ -d "/mnt/c/Users/bwang/Downloads/vscode/bin" ]] && path=("/mnt/c/Users/bwang/Downloads/vscode/bin" $path)
+
 # Linuxbrew
 if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
   path=(/home/linuxbrew/.linuxbrew/bin $path)
