c7c62026f3de4d6b63861fe7927ec051dc62fd15
Bin Wang <bin.wang@toptal.com>
2026-09-25 09:41:43 +1000

    vim tutor skill

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    -- Go to definition
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    
+    -- Optional but highly recommended additions:
+    -- Go to declaration
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    -- Show hover documentation
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  end,
+})
